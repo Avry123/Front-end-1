c70938c3dfc8c0bfe41e89e865fa0a54de7f45af
chatbox9321 <85362220+chatbox9321@users.noreply.github.com>
2022-08-23 12:17:39 +0530

    Second Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link href="https://fonts.googleapis.com/css2?family=Arimo:ital@1&family=Fredoka:wght@300&family=League+Gothic&family=League+Spartan&family=Outfit:wght@400;700&family=Rowdies:wght@300&family=Source+Sans+Pro&display=swap" rel="stylesheet"></link>
       </Head>
       <Card />  
